e1d3d4d0741c50d0c946b53a2b8557a508295d13
João do Nascimento <69540048+JoaoAN2@users.noreply.github.com>
2020-11-15 00:57:31 -0300

    teste

---
+++ src/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const pages = require("./pages.js")
+
+//Teste
+const server = express();
+
+server
+.use(express.urlencoded({extended: true}))
+
+.use(express.static("public"))
+.set("views",path.join(__dirname,"views"))
+.set("view engine", "hbs")
+
+//Rotas
+.get('/',pages.index)
+.get('/orphanage',pages.orphanage)
+.get('/orphanages',pages.orphanages)
+.get('/create-orphanage',pages.createOrphanage)
+.post('/save-orphanage', pages.saveOrphanage)
+
+server.listen(5500)
